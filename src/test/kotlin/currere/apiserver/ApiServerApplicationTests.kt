cd5db813ac4861565b4a90cbf9ed572eb607af86
k-kbk <kkbk0077@gmail.com>
2024-07-12 13:27:22 +0900

    [chore] 프로젝트 초기 설정

---
+++ src/test/kotlin/currere/apiserver/ApiServerApplicationTests.kt
@@ -0,0 +1,11 @@
+package currere.apiserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiServerApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
