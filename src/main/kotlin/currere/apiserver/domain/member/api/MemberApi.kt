07e37f55f525f5fd4f7a0041d308ff1a2ec4fe72
k-kbk <kkbk0077@gmail.com>
2024-07-14 17:15:08 +0900

    [feat] 회원가입 api interface

--- src/main/kotlin/currere/apiserver/domain/member/api/MemberApi.kt
+++ src/main/kotlin/currere/apiserver/domain/member/api/MemberApi.kt
@@ -1,6 +1,17 @@
 package currere.apiserver.domain.member.api
 
+import currere.apiserver.domain.member.dto.request.SigninRequest
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Tag(name = "Member API")
-interface MemberApi
+interface MemberApi {
+
+    @Operation(summary = "회원가입")
+    @PostMapping("/signin")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    fun signin(request: SigninRequest)
+}
--- src/main/kotlin/currere/apiserver/domain/member/api/MemberController.kt
+++ src/main/kotlin/currere/apiserver/domain/member/api/MemberController.kt
@@ -1,8 +1,15 @@
 package currere.apiserver.domain.member.api
 
+import currere.apiserver.domain.member.dto.request.SigninRequest
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/members")
-class MemberController : MemberApi
+class MemberController : MemberApi {
+    override fun signin(@Valid @RequestBody request: SigninRequest) {
+        TODO("Not yet implemented")
+    }
+}
