fe3e213aadc724629315637f799fbe712b2f594f
k-kbk <kkbk0077@gmail.com>
2024-07-14 15:43:40 +0900

    [chore] 기타 수정

--- src/main/kotlin/currere/apiserver/domain/member/application/MemberService.kt
+++ src/main/kotlin/currere/apiserver/domain/member/application/MemberService.kt
@@ -1,7 +1,6 @@
 package currere.apiserver.domain.member.application
 
-import currere.apiserver.domain.member.api.MemberApi
 import org.springframework.stereotype.Service
 
 @Service
-class MemberService : MemberApi
+class MemberService
