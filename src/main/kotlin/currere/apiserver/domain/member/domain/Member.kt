2e170e6b7fec97d3439c6d0b1229bf3287aee1ca
k-kbk <kkbk0077@gmail.com>
2024-07-15 16:57:08 +0900

    [feat] Member

--- src/main/kotlin/currere/apiserver/domain/member/domain/Member.kt
+++ src/main/kotlin/currere/apiserver/domain/member/domain/Member.kt
@@ -1,9 +1,22 @@
 package currere.apiserver.domain.member.domain
 
 import currere.apiserver.global.common.BaseEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "member")
-class Member : BaseEntity()
+class Member(
+    @Column(nullable = true)
+    val studentId: String,
+
+    @Column(nullable = true)
+    val name: String,
+
+    @Column(nullable = true)
+    val email: String,
+
+    @Column(nullable = true)
+    val password: String,
+) : BaseEntity()
