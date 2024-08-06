3e515ac5a98638e3b24ba56a7b651417e5ea19f8
k-kbk <kkbk0077@gmail.com>
2024-08-06 14:46:01 +0900

    [feat] DiagnoseRequest 유효성 검사

--- src/main/kotlin/fatpet/apiserver/domain/diagnosis/dto/request/DiagnoseRequest.kt
+++ src/main/kotlin/fatpet/apiserver/domain/diagnosis/dto/request/DiagnoseRequest.kt
@@ -1,9 +1,14 @@
 package fatpet.apiserver.domain.diagnosis.dto.request
 
+import jakarta.validation.constraints.Min
+
 data class DiagnoseRequest(
-    val weight: Float = 0F,
+    @field:Min(value = 1, message = "체중은 0보다 커야 합니다.")
+    val weight: Float,
 
-    val neckSize: Float = 0F,
+    @field:Min(value = 1, message = "목 둘레는 0보다 커야 합니다.")
+    val neckSize: Float,
 
-    val chestSize: Float = 0F,
+    @field:Min(value = 1, message = "가슴 둘레는 0보다 커야 합니다.")
+    val chestSize: Float,
 )
